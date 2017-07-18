9e04590dca7aba920113a8372b4c7cf68a6b634f
Austin Jones <ajonestx@gmail.com>
2017-07-17 22:32:06 -0600

    add server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(8080, function() {
+    console.log("Listening on 8080")
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/views/pages/index.html');
+});
