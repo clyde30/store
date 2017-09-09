afaa81cd5bd656049ab9917ed27b6f92a9d60fc5
Austin Jones <ajonestx@gmail.com>
2017-09-09 12:52:30 -0600

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var nodemon = require('gulp-nodemon');
 
 // Static Server + watching scss/html files
 gulp.task('browser-sync', ['sass'], function () {
@@ -9,6 +10,14 @@ gulp.task('browser-sync', ['sass'], function () {
         server: "./app"
     });
 });
+
+gulp.task('start', function () {
+    nodemon({
+        script: 'server.js'
+        , ext: 'js html'
+        , env: { 'NODE_ENV': 'development' }
+    });
+})
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function () {
@@ -24,4 +33,4 @@ gulp.task('watch', function () {
     gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('default', ['watch', 'sass', 'browser-sync']);
+gulp.task('dev', ['watch', 'sass', 'browser-sync']);
